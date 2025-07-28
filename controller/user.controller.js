43e2cec783c513a94af55c7cea12dbe2d11e266b
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-07-28 02:54:55 +0100

    update login

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,10 +4,10 @@ const saltRounds = 10;
 
 const userController = {}
 
-userController.createUser = async (req, res) => {
+userController.createUser = async (req, res) => {   
     try {
         const {name, email, password} = req.body;
-        const user = await User.findOne({email});
+        const user = await User.findOne({email}, "-createdAt updatedAt -__v");
         if (user) {
             throw new Error('User already exists');
         }
@@ -18,6 +18,23 @@ userController.createUser = async (req, res) => {
         res.status(200).json({status:"success"});
     } catch (error) {
         res.status(400).json({status:"fail", error})
+    };
+};
+
+userController.loginWithEmail = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if (user) {
+            const isMatch = bcrypt.compareSync(password, user.password);
+            if (isMatch) {
+                const token = user.generateToken();
+                return res.status(200).json({status:"success", user, token});
+            }
+        }
+        throw new Error('Invalid email or password');
+    } catch (error) {
+        res.status(400).json({status:"fail", error});
     }
 }
 
